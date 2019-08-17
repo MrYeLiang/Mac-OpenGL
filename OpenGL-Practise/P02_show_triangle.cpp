78e821dbe7d16f7c1eb0abf9fc7c49fffaac945c
叶亮 <yeliang@tv365.net>
2019-08-17 14:44:26 +0800

    使用EBO绘制多个三角形，即四边形

--- OpenGL-Practise/P02_show_triangle.cpp
+++ OpenGL-Practise/P02_show_triangle.cpp
@@ -109,26 +109,39 @@ int show_triangle()
     glDeleteShader(fragmentShader);
     
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, //left
-        0.5f,  -0.5f, 0.0f, //Right
-        0.0f,  0.5f, 0.0f   //Top
+        0.5f,  0.5f,  0.0f,   //Top Right
+        0.5f,  -0.5f, 0.0f,  //Bottom Right
+        -0.5f, -0.5f, 0.0f, //Bottom Left
+        -0.5f, 0.5f,  0.0f
     };
     
+    GLint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+ 
     //顶点缓冲对象
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    
+    //线框模式
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -140,7 +153,8 @@ int show_triangle()
         //draw first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
         
         //swap the screen buffers
@@ -148,6 +162,7 @@ int show_triangle()
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
     //Terminate GLFW, clearing any resources allocated by GLFW
     glfwTerminate();
