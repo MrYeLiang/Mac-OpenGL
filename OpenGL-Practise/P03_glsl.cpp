6b849f6c78885747de05ccba5a98a136b515d4c2
叶亮 <yeliang@tv365.net>
2019-08-21 16:17:58 +0800

    GLSL----将把三角形的三个角分别指定为红色、绿色和蓝色

--- OpenGL-Practise/P03_glsl.cpp
+++ OpenGL-Practise/P03_glsl.cpp
@@ -28,20 +28,22 @@ using namespace std;
 const char *vShaderSource =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
         "void main()\n"
         "{\n"
         "gl_Position = vec4(aPos, 1.0);\n"
+        "ourColor = aColor;\n"
         "}\0";
 
 const char *fShaderSource =
         "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 ourColor;\n"
         "void main()\n"
         "{\n"
-        "FragColor = ourColor;\n"
+        "FragColor = vec4(ourColor, 1.0f);\n"
         "}\n\0";
-
 
 int dynamic_set_color()
 {
@@ -114,9 +116,10 @@ int dynamic_set_color()
     
     //set up vertex data
     float vertices[] = {
-        0.5f,  -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        //position           //color
+        0.5f,  -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+        0.0f,   0.5f, 0.0f,  0.0f, 0.0f, 1.0f
     };
     
     unsigned int VBO, VAO;
@@ -128,26 +131,24 @@ int dynamic_set_color()
     //1 bind the vertex Array, 2 bind vertex buffer, 3 configure vertex attributes
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     
+    //position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     
-    glBindVertexArray(VAO);
+    //color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)( 3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
+    glUseProgram(shaderProgram);
     while(!glfwWindowShouldClose(window)){
         processInput(window);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        glUseProgram(shaderProgram);
-        
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-        
         //render the triangle
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
