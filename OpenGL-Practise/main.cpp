57a512d669af023d2be66c27a42b5ddf26b7b477
叶亮 <yeliang@tv365.net>
2019-08-21 17:46:58 +0800

    调整文件结构

--- OpenGL-Practise/main.cpp
+++ OpenGL-Practise/main.cpp
@@ -5,9 +5,9 @@
 //  Created by 叶亮 on 2019/8/7.
 //  Copyright © 2019 叶亮. All rights reserved.
 //
-#include "P01_create_window.hpp"
-#include "P02_show_triangle.hpp"
-#include "P03_glsl.hpp"
+#include "P01_window/P01_create_window.hpp"
+#include "P02_triangle/P02_show_triangle.hpp"
+#include "P03_glsl/P03_glsl.hpp"
 int main(){
     //1 显示窗口
     //show_window();
